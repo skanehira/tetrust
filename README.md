86c897437a51f98e17dcec19865e98ebf746236f
skanehira <sho19921005@gmail.com>
2023-02-11 11:46:04 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# tetrust
+Rustで作るテトリス風ゲーム入門
+
+https://zenn.dev/kumavale/books/30efec2e1d3428
---
+++ src/main.rs
@@ -0,0 +1,226 @@
+#![allow(dead_code)]
+
+use getch_rs::{Getch, Key};
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+use std::sync::{Arc, Mutex};
+use std::{thread, time};
+
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+impl Distribution<BlockKind> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> BlockKind {
+        match rng.gen_range(0..=6) {
+            0 => BlockKind::I,
+            1 => BlockKind::O,
+            2 => BlockKind::S,
+            3 => BlockKind::Z,
+            4 => BlockKind::J,
+            5 => BlockKind::L,
+            _ => BlockKind::T,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
+type BlockShape = [[usize; 4]; 4];
+
+const BLOCKS: [BlockShape; 7] = [
+    // ■■■■
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    // ■■
+    // ■■
+    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+    //  ■■
+    // ■■
+    [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]],
+    // ■■
+    //  ■■
+    [[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+    // ■
+    // ■■■
+    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    //   ■
+    // ■■■
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    //  ■
+    // ■■■
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+];
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if y + pos.y >= FIELD_HEIGHT || x + pos.x >= FIELD_WIDTH {
+                continue;
+            }
+            if field[y + pos.y][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn draw(field: &Field, pos: &Position, block: BlockKind) {
+    let mut field_buf = field.clone();
+
+    for y in 0..4 {
+        for x in 0..4 {
+            if BLOCKS[block as usize][y][x] == 1 {
+                field_buf[y + pos.y][x + pos.x] = 1;
+            }
+        }
+    }
+
+    println!("\x1b[H");
+    for y in 0..FIELD_HEIGHT {
+        for x in 0..FIELD_WIDTH {
+            if field_buf[y][x] == 1 {
+                print!("[]");
+            } else {
+                print!("  ");
+            }
+        }
+        println!();
+    }
+}
+
+fn main() {
+    let field = Arc::new(Mutex::new([
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ]));
+
+    let pos = Arc::new(Mutex::new(Position { x: 4, y: 0 }));
+    let block = Arc::new(Mutex::new(rand::random::<BlockKind>()));
+
+    println!("\x1b[2J\x1b[H\x1b[?25l");
+    draw(
+        &field.lock().unwrap(),
+        &pos.lock().unwrap(),
+        *block.lock().unwrap(),
+    );
+
+    {
+        let pos = Arc::clone(&pos);
+        let field = Arc::clone(&field);
+        let block = Arc::clone(&block);
+        let _ = thread::spawn(move || loop {
+            thread::sleep(time::Duration::from_millis(300));
+            let mut pos = pos.lock().unwrap();
+            let mut field = field.lock().unwrap();
+            let mut block = block.lock().unwrap();
+            let new_pos = Position {
+                x: pos.x,
+                y: pos.y + 1,
+            };
+
+            if !is_collision(&field, &new_pos, *block) {
+                *pos = new_pos;
+            } else {
+                for y in 0..4 {
+                    for x in 0..4 {
+                        if BLOCKS[*block as usize][y][x] == 1 {
+                            field[y + pos.y][x + pos.x] = 1;
+                        }
+                    }
+                }
+                *pos = Position { x: 4, y: 0 };
+                *block = rand::random();
+            }
+            draw(&field, &pos, *block);
+        });
+    }
+
+    let g = Getch::new();
+    loop {
+        match g.getch() {
+            Ok(Key::Left) => {
+                let mut pos = pos.lock().unwrap();
+                let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
+                let new_pos = Position {
+                    x: pos.x.checked_sub(1).unwrap_or_else(|| pos.x),
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, *block) {
+                    *pos = new_pos;
+                }
+                draw(&field, &pos, *block);
+            }
+            Ok(Key::Down) => {
+                let mut pos = pos.lock().unwrap();
+                let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
+                let new_pos = Position {
+                    x: pos.x,
+                    y: pos.y + 1,
+                };
+                if !is_collision(&field, &new_pos, *block) {
+                    // posの座標を更新
+                    *pos = new_pos;
+                }
+                draw(&field, &pos, *block);
+            }
+            Ok(Key::Right) => {
+                let mut pos = pos.lock().unwrap();
+                let field = field.lock().unwrap();
+                let block = block.lock().unwrap();
+                let new_pos = Position {
+                    x: pos.x + 1,
+                    y: pos.y,
+                };
+                if !is_collision(&field, &new_pos, *block) {
+                    // posの座標を更新
+                    *pos = new_pos;
+                }
+                draw(&field, &pos, *block);
+            }
+            Ok(Key::Char('q')) => {
+                // カーソルを再表示
+                println!("\x1b[?25h");
+                return;
+            }
+            _ => (),
+        }
+    }
+}
