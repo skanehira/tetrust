96cc0122e61237ba6dd6d1b08e58951b88386bc3
skanehira <sho19921005@gmail.com>
2023-02-12 21:00:35 +0900

    update

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,7 @@
-use crate::block::{BlockKind, BLOCKS};
+use crate::block::{BlockKind, BlockShape, BLOCKS};
 
-pub const FIELD_WIDTH: usize = 11 + 2;
-pub const FIELD_HEIGHT: usize = 20 + 1;
+pub const FIELD_WIDTH: usize = 11 + 2 + 1;
+pub const FIELD_HEIGHT: usize = 20 + 1 + 1;
 pub type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
 
 pub struct Position {
@@ -11,55 +11,56 @@ pub struct Position {
 
 impl Default for Position {
     fn default() -> Self {
-        Position { x: 4, y: 0 }
+        Position { x: 5, y: 0 }
     }
 }
 
 pub struct Game {
     pub field: Field,
     pub pos: Position,
-    pub block: BlockKind,
+    pub block: BlockShape,
 }
 
 impl Default for Game {
     fn default() -> Self {
         Game {
             field: [
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             ],
-            pos: Position { x: 4, y: 0 },
-            block: rand::random::<BlockKind>(),
+            pos: Position::default(),
+            block: BLOCKS[rand::random::<BlockKind>() as usize],
         }
     }
 }
 
-pub fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+pub fn is_collision(field: &Field, pos: &Position, block: &BlockShape) -> bool {
     for y in 0..4 {
         for x in 0..4 {
-            if y + pos.y >= FIELD_HEIGHT || x + pos.x >= FIELD_WIDTH {
+            if y + pos.y >= FIELD_HEIGHT - 1 || x + pos.x >= FIELD_WIDTH - 1 {
                 continue;
             }
-            if field[y + pos.y][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+            if field[y + pos.y][x + pos.x] & block[y][x] == 1 {
                 return true;
             }
         }
@@ -73,15 +74,15 @@ pub fn draw(Game { field, pos, block }: &Game) {
 
     for y in 0..4 {
         for x in 0..4 {
-            if BLOCKS[*block as usize][y][x] == 1 {
+            if block[y][x] == 1 {
                 field_buf[y + pos.y][x + pos.x] = 1;
             }
         }
     }
 
     println!("\x1b[H");
-    for y in 0..FIELD_HEIGHT {
-        for x in 0..FIELD_WIDTH {
+    for y in 0..FIELD_HEIGHT - 1 {
+        for x in 0..FIELD_WIDTH - 1 {
             if field_buf[y][x] == 1 {
                 print!("[]");
             } else {
@@ -95,7 +96,7 @@ pub fn draw(Game { field, pos, block }: &Game) {
 pub fn fix_block(Game { field, pos, block }: &mut Game) {
     for y in 0..4 {
         for x in 0..4 {
-            if BLOCKS[*block as usize][y][x] == 1 {
+            if block[y][x] == 1 {
                 field[y + pos.y][x + pos.x] = 1;
             }
         }
@@ -103,7 +104,7 @@ pub fn fix_block(Game { field, pos, block }: &mut Game) {
 }
 
 pub fn erase_line(field: &mut Field) {
-    for y in 1..FIELD_HEIGHT - 1 {
+    for y in 1..FIELD_HEIGHT - 2 {
         let mut can_erase = true;
         for x in 1..FIELD_WIDTH - 1 {
             if field[y][x] == 0 {
@@ -120,7 +121,52 @@ pub fn erase_line(field: &mut Field) {
 }
 
 pub fn move_block(game: &mut Game, new_pos: Position) {
-    if !is_collision(&game.field, &new_pos, game.block) {
+    if !is_collision(&game.field, &new_pos, &game.block) {
         game.pos = new_pos;
+    }
+}
+
+pub fn spawn_block(game: &mut Game) -> Result<(), ()> {
+    game.pos = Position::default();
+    game.block = BLOCKS[rand::random::<BlockKind>() as usize];
+
+    if is_collision(&game.field, &game.pos, &game.block) {
+        Err(())
+    } else {
+        Ok(())
+    }
+}
+
+pub fn gameover(game: &Game) -> ! {
+    draw(game);
+    println!("GAMEOVER");
+    quit();
+}
+
+pub fn quit() -> ! {
+    println!("\x1b[?25h");
+    std::process::exit(0);
+}
+
+pub fn rotate_right(game: &mut Game) {
+    let mut new_shape: BlockShape = Default::default();
+    for y in 0..4 {
+        for x in 0..4 {
+            new_shape[y][x] = game.block[4 - 1 - x][y];
+        }
+    }
+    if !is_collision(&game.field, &game.pos, &new_shape) {
+        game.block = new_shape;
+    }
+}
+pub fn rotate_left(game: &mut Game) {
+    let mut new_shape: BlockShape = Default::default();
+    for y in 0..4 {
+        for x in 0..4 {
+            new_shape[4 - 1 - x][y] = game.block[y][x];
+        }
+    }
+    if !is_collision(&game.field, &game.pos, &new_shape) {
+        game.block = new_shape;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 mod block;
 mod game;
 
-use game::{draw, erase_line, fix_block, is_collision, move_block, Game, Position};
+use game::{
+    draw, erase_line, fix_block, gameover, is_collision, move_block, quit, rotate_left,
+    rotate_right, spawn_block, Game, Position,
+};
 use getch_rs::{Getch, Key};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
@@ -22,15 +25,17 @@ fn main() {
                 y: game.pos.y + 1,
             };
 
-            if !is_collision(&game.field, &new_pos, game.block) {
+            if !is_collision(&game.field, &new_pos, &game.block) {
                 game.pos = new_pos;
             } else {
                 // ブロックを固定
                 fix_block(&mut game);
                 // 行削除
                 erase_line(&mut game.field);
-                game.pos = Position { x: 4, y: 0 };
-                game.block = rand::random();
+
+                if spawn_block(&mut game).is_err() {
+                    gameover(&game);
+                }
             }
             draw(&game);
         });
@@ -66,10 +71,18 @@ fn main() {
                 move_block(&mut game, new_pos);
                 draw(&game);
             }
+            Ok(Key::Char('x')) => {
+                let mut game = game.lock().unwrap();
+                rotate_right(&mut game);
+                draw(&game);
+            }
+            Ok(Key::Char('z')) => {
+                let mut game = game.lock().unwrap();
+                rotate_left(&mut game);
+                draw(&game);
+            }
             Ok(Key::Char('q')) => {
-                // カーソルを再表示
-                println!("\x1b[?25h");
-                return;
+                quit();
             }
             _ => (),
         }
